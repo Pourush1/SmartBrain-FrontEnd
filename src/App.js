2c0d45591982b1365442e1d0b3d70405cacf1bc9
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2019-08-03 08:38:51 -0500

    All files sent right now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,158 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+import SignIn from "./components/SignIn/SignIn";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// initialize with your api key. This will also work in your browser via http://browserify.org/
+
+const app = new Clarifai.App({
+  apiKey: "f52c3fbe9c8c4016a4d04caf9bcd95c7"
+});
+
+const particlesOptions = {
+  // particles: {
+  //   number: {
+  //     value: 30,
+  //     density: {
+  //       enable: true,
+  //       value_area: 800
+  //     }
+  //   }
+  // }
+  particles: {
+    number: {
+      value: 160,
+      density: {
+        enable: false
+      }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        speed: 4,
+        size_min: 0.3
+      }
+    },
+    line_linked: {
+      enable: false
+    },
+    move: {
+      random: true,
+      speed: 1,
+      direction: "top",
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "bubble"
+      },
+      onclick: {
+        enable: true,
+        mode: "repulse"
+      }
+    },
+    modes: {
+      bubble: {
+        distance: 250,
+        duration: 2,
+        size: 0,
+        opacity: 0
+      },
+      repulse: {
+        distance: 400,
+        duration: 4
+      }
+    }
+  }
+};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageLinkUrl: "",
+      box: {},
+      route: "signin"
+    };
+  }
+
+  calculateFaceLocation = data => {
+    const clarifyFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    //this object is what going to fill the box object
+    return {
+      leftCol: clarifyFace.left_col * width,
+      topRow: clarifyFace.top_row * height,
+      rightCol: width - clarifyFace.right_col * width,
+      bottomRow: height - clarifyFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({ box: box });
+  };
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+    // console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageLinkUrl: this.state.input });
+    // Predict the contents of an image by passing in a URL.
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => {
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  onRouteChange = () => {
+    this.setState({ route: "home" });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+
+        {this.state.route === "signin" ? (
+          <SignIn onRouteChange={this.onRouteChange} />
+        ) : (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              imageLinkUrl={this.state.imageLinkUrl}
+              box={this.state.box}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="f3 white">{"Pourush, your current rank is ...."}</div>
+      <div className="f1 white">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
